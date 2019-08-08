330952496928d04ea47ce52f3692be494b0abef4
fradkin <fradkin@microfocus.com>
2019-08-08 09:52:20 +0300

    defect #1002: add test that fails on BeforeClass

---
+++ webapp/src/test/java/com/hp/devops/demoapp/BeforeClassTest.java
@@ -0,0 +1,24 @@
+package com.hp.devops.demoapp;
+
+import org.junit.*;
+
+/**
+ * check reports in case of failures in BeforeClass
+ */
+public class BeforeClassTest {
+
+	@BeforeClass
+	public static void beforeAll() {
+		Assert.assertNotNull(null);
+	}
+
+	@Test
+	public void passed1() {
+		Assert.assertNotNull("aaa");
+	}
+
+	@Test
+	public void passed2() {
+		Assert.assertNotNull("aaa");
+	}
+}
