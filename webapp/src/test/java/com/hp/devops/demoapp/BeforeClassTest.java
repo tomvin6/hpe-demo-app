5267d8a143e98d46f559d2a79eaff23ca7fb4568
fradkin <fradkin@microfocus.com>
2019-08-11 10:16:26 +0300

    defect #1002: commit1

--- webapp/src/test/java/com/hp/devops/demoapp/BeforeClassTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/BeforeClassTest.java
@@ -6,20 +6,11 @@ import org.junit.*;
  * check reports in case of failures in BeforeClass
  */
 public class BeforeClassTest {
-//    int counter = 0;
 
 	@BeforeClass
 	public static void beforeAll() {
 		Assert.assertNotNull(null);
 	}
-//
-//	@Before
-//	public void beforeEach() {
-//	    if (counter == 0) {
-//            Assert.assertNotNull(null);
-//        }
-//		counter++;
-//	}
 
 	@Test
 	public void passed1() {
